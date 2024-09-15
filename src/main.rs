04c3416fffa810e911d800d79fecbbca6b0dd484
223880 <rafael.segala45@gmail.com>
2024-09-15 06:33:28 -0300

    Fix Cargo

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use bitcoin::blockdata::block::BlockHeader;
 use bitcoin::hashes::sha256d::Hash;
-use bitcoin::TaprootMerkleBranch;
 
-struct SidechainBlockHeader {
+struct SidechainBlock{
     version: u32,
     prev_blockhash: Hash,
     merkle_root: Hash,
     time: u32,
     bits: u32,
     nonce: u32,
-    taproot: u128,
+    taproot_merkele_branch: u128,
     aux_pow: AuxPoW,  // Data structure for AuxPoW
 }
 
 struct AuxPoW {
-    bitcoin_block_header: BlockHeader,  // Bitcoin block header to prove shared PoW
     coinbase_transaction: Vec<u8>,      // Coinbase transaction referencing sidechain work
     merkle_branch: Vec<Hash>,           // Merkle branch linking sidechain to Bitcoin block
 }
